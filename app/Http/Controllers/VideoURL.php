57f7d6fb57830d799200848acef57e141246c78b
Michael Frank <maickomtutos@gmail.com>
2020-09-07 17:32:58 -0300

    change files

--- app/Http/Controllers/VideoURL.php
+++ app/Http/Controllers/VideoURL.php
@@ -4,22 +4,19 @@ namespace App\Http\Controllers;
 
 use Goutte\Client;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpClient\HttpClient;
 
 class VideoURL extends Controller
 {
 	public function FInstaVD(Request $req)
 	{
-		try {
-			$cl = new Client(HttpClient::create(['timeout' => 120]));
+	
+			$cl = new Client();
 			$urlr = $req->url;
 			$craw = $cl->request('GET', $urlr);
 			$res = $craw->filterXpath('//meta[@property="og:video"]')->attr('content');
 			$ar = array('urlbakc' => $res);
 			return json_encode($ar, JSON_UNESCAPED_SLASHES);
-		} catch (\Throwable $th) {
-			throw $th;
-		}
+		
 	}
 	public function FInstaIMG(Request $req)
 	{
